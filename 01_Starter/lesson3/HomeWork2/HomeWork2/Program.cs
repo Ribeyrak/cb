d4401a727beba3f002107b9dde571b6509ba43df
Evhen <ev.lukhtan@gmail.com>
2018-01-11 18:49:25 +0200

    first project clean

---
+++ 01_Starter/lesson3/HomeWork2/HomeWork2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 6;
+            int b = 2;
+            int c = 0;
+
+            a = b + c + 1;
+            c = (a * b - 10) * 10;
+
+            Console.WriteLine(a);
+            Console.WriteLine(c);
+
+            Console.ReadKey();
+        }
+    }
+}
