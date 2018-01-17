6da96419c07135fe2cf72ebd48dd0dbe657801ca
Evhen <ev.lukhtan@gmail.com>
2018-01-17 23:54:34 +0200

    first project clean

---
+++ 01_Starter/Новая папка/deMorgana/deMorgana/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace deMorgana
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool A = false;
+            bool B = false;
+
+            if (A || B)
+                Console.WriteLine("A || B = {0}", A || B);
+            else
+                Console.WriteLine("A || B = {0}", A || B);
+
+            if (!(!A && !B))
+                Console.WriteLine("!(!A && !B = {0}", !(!A && !B));
+            else
+            Console.WriteLine("!(!A && !B = {0}", !(!A && !B));
+
+            Console.ReadKey();
+        }
+    }
+}
+//Используя теорему Де Моргана, преобразуйте исходное выражение A | B, в эквивалентное выражение
