b4f4869df025662d19c758d50b16b4a57a7d248d
Evhen <ev.lukhtan@gmail.com>
2018-01-18 01:48:01 +0200

    first project clean

---
+++ 01_Starter/lesson5/task2/task2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Security;
+using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //byte operand = 0x01; //1111 0000
+            //byte mask = 0xFD;     //1111 1101 
+            //Console.WriteLine("operand = {0:X}", operand);
+            //
+            //operand = (byte) (operand | mask);
+            //Console.WriteLine("operand = {0:X}", operand);
+            Console.WriteLine("Введите число:");
+            int s = Convert.ToInt32(Console.ReadLine());
+
+            byte operand1 = Console.ReadLine();
+            int result;
+
+            if 
+
+            operand1 = 0x00;
+            operand2 = 0x02;
+            result = operand1 | operand2;
+
+            //Console.WriteLine("{0} ")
+
+            Console.ReadKey();
+        }
+    }
+}
+//Известно, что у четных чисел младший бит имеет значение 0 
+//Напишите программу, которая будет выполнять проверку чисел на четность.
+//Предложите два варианта решения поставленной задачи.
