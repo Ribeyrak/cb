051430fca7bf9dcc7b5ec7cf66d0a613d54c30d7
Evhen <ev.lukhtan@gmail.com>
2018-01-21 16:18:59 +0200

    first project clean

---
+++ 01_Starter/lesson6/task1/task1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int a = 0; a <= 10; a++)
+            {
+                for (int b = 0; b <= 12; b++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+            }
+               
+            Console.ReadKey();
+        }
+    }
+}
+//Задание Используя Visual Studio, создайте проект по шаблону Console Application.
+//Создайте две целочисленные переменные и задайте им некоторые значения.
+//Применяя технику вложенных цикл ов, нарисуйте прямоугольник из звездочек.Используйте значения ранее созданных
+//переменных для указания высоты и ширин ы прямоугольника.
---
+++ 01_Starter/notepad/notepad/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace notepad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OpenStandardError();
+            Console.ReadKey();
+        }
+    }
+}
