ff702516968cd6392fc507ac2bb80eb827b1bc35
Evhen <ev.lukhtan@gmail.com>
2018-01-24 19:30:59 +0200

    first project clean

---
+++ 01_Starter/lesson7/Calculate/Calculate/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculate
+{
+    class Program
+    {
+        static int Calculate(int a, int b, int c)
+        {
+            return ((a + b + c) / 2);
+        }
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое число:");
+            Console.ReadLine();
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите второе число:");
+            Console.ReadLine();
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите третье число:");
+            Console.ReadLine();
+            int c = Convert.ToInt32(Console.ReadLine());
+
+            int sum = Calculate(a,b,c);
+            Console.WriteLine("Срезнее арифметическое значение равно: {0}", sum);
+
+            Console.ReadKey();
+        }
+    }
+}
+//Создайте метод с именем Calculate, который принимает в качестве параметров три целочисленных аргумента и выводит на экран среднее арифметическое значений аргументов.
