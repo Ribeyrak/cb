e4e3d417bd7e5d404d629bbeb9f1acc4a42bffb1
Evhen <ev.lukhtan@gmail.com>
2018-01-24 21:35:34 +0200

    first project clean

---
+++ 01_Starter/lesson7/Converter/Converter/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Converter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите сумму для конвертации: ");
+            int summ = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите курс валюты: ");
+            int course = Convert.ToInt32(Console.ReadKey());
+
+            int ekv = summ * course;
+            Console.WriteLine("При курсе {0} за {1} единиц вы получите {2}", course, summ, ekv);
+        }
+    }
+}
