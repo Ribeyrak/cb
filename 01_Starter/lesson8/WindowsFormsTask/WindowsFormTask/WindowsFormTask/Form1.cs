0f40d8c41511a736aafd5fa73fb3ed2b81196678
Evhen <ev.lukhtan@gmail.com>
2018-01-27 20:41:00 +0200

    first project clean

---
+++ 01_Starter/lesson8/WindowsFormsTask/WindowsFormTask/WindowsFormTask/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormTask
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked == true)
+            {
+                textBox3.Text = Convert.ToString(Convert.ToInt32(textBox1.Text) % Convert.ToInt32(textBox2.Text));
+            }
+
+            if (radioButton2.Checked == true)
+            {
+                textBox3.Text = Convert.ToString(Math.Pow(Convert.ToInt32(textBox1.Text), Convert.ToInt32(textBox2.Text)));
+            }
+
+            if (radioButton3.Checked == true)
+            {
+                textBox3.Text = textBox1.Text + textBox2.Text;
+            }
+
+            if (radioButton4.Checked == true)
+            {
+                textBox3.Text = Convert.ToString(Convert.ToDouble(textBox1.Text) / Convert.ToDouble(textBox2.Text));
+            }
+        }
+    }
+}
