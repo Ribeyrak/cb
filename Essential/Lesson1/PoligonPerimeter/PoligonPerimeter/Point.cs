31e38c7ed040d2f3a5fd59e8b66cd9555be95bf3
Evhen <ev.lukhtan@gmail.com>
2018-02-04 20:44:43 +0200

    first project clean

---
+++ Essential/Lesson1/PoligonPerimeter/PoligonPerimeter/Point.cs
@@ -0,0 +1,36 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+namespace PoligonPerimeter
+{
+    class Point
+    {
+        int a, b;
+        string field;
+
+        public int A
+        {
+            get { return a; }
+        }
+
+        public int B
+        {
+            get { return b; }
+        }
+
+        public string Field
+        {
+            get { return field; }
+        }
+
+        public Point(int a, int b, string field)
+        {
+            this.a = a;
+            this.b = b;
+            this.field = field;
+        }
+    }
+}
