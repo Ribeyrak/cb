6f148d085d32ce70ee21d829e33a53f08b4729a1
Evhen <ev.lukhtan@gmail.com>
2018-02-06 15:56:01 +0200

    first project clean

--- Essential/Lesson2/Converter/Converter/Program.cs
+++ Essential/Lesson2/Converter/Converter/Program.cs
@@ -8,6 +8,26 @@ namespace Converter
 {
     class Converter
     {
+        //Поля
+        private double usd = 28.5, eur = 31, rub = 0.5;
+
+        //Свойства
+        public double USD
+        {
+            get { return usd; }
+        }
+        
+        public double EUR
+        {
+            get { return eur; }
+        }
+
+        public double RUB
+        {
+            get { return rub; }
+        }
+
+        //Созаю конструктор
         public Converter (double usd, double eur, double rub)
         {
 
@@ -30,7 +50,7 @@ namespace Converter
 }
 //(+)Используя Visual Studio, создайте проект по шаблону Console Application.
 //Требуется: Создать класс Converter.В теле класса создать пользовательский конструктор, 
-//который принимает три вещественных аргумента, и инициализирует поля соответствующие курсу 3 -х основн ых валют, 
+//который принимает три вещественных аргумента, и инициализирует поля соответствующие курсу 3 -х основных валют, 
 //по отношению к гривне - public Converter(double usd, double eur, double rub). 
 //Написать программу, которая будет выполнять конвертацию из гривны в одну из указанных валют,
 //также программа должна производить конвертацию из указанных валют в гривну.
