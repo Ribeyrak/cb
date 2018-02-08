b16fc600ba1d61a19d5cd329fadd221495436e21
Evhen <ev.lukhtan@gmail.com>
2018-02-08 22:37:35 +0200

    first project clean

--- Essential/Lesson3/TransportInfo/TransportInfo/Vehicle.cs
+++ Essential/Lesson3/TransportInfo/TransportInfo/Vehicle.cs
@@ -8,5 +8,73 @@ namespace TransportInfo
 {
     class Vehicle
     {
+        int price, speed, data;
+
+        protected int Price
+        {
+            get { return price; }
+        }
+
+        protected int Speed
+        {
+            get { return speed; }
+        }
+
+        public int Data
+        {
+            get { return data; }
+        }
+    }
+
+    public Vehicle(int price, int speed, int data)
+    {
+        this.price = price;
+        this.speed = speed;
+        this.data = data;
+    }
+
+
+    class Plane : Vehicle
+    {
+        public Plane(int price, int speed, int data) : base(price, speed, data) { }
+
+        private int high;
+        public int High
+        {
+            get { return high; }
+            set { high = value; }
+        }
+    }
+
+    class Car : Vehicle
+    {
+        public Car(int price, int speed, int data)
+           : base(price, speed, data)
+        {
+
+        }
+    }
+
+    class Ship : Vehicle
+    {
+        public Ship(int price, int speed, int data) : base(price, speed, data)
+        {
+
+        }
+
+        private int passanger;
+        public int Passanger
+        {
+            get { return passanger; }
+            set { passanger = value; }
+        }
+
+        private string port;
+        public string Port
+        {
+            get { return port;}
+            set { port = value; }
+        }
+
     }
 }
