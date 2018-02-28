adc69c12ccb1865ed61c24bbc9aa502d55894c35
Ribeyrak <ev.lukhtan@gmail.com>
2018-02-28 18:20:17 +0200

    task completion

---
+++ Essential/Lesson6_Static/Delegation_Book/Delegation_Book/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Delegation_Book
+{
+    internal class Book
+    {
+        public void FindNext(string str)
+        {
+            Console.WriteLine("Find a line " + str);
+        }
+    }
+
+    internal static class FindAndReplaceManager
+    {
+        public static void FindNext(string str)
+        {
+            var book = new Book();
+            book.FindNext(str);
+        }
+    }
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            FindAndReplaceManager.FindNext("test");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Essential/Lesson6_Static/Delegation_Book/Delegation_Book/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("Delegation_Book")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Delegation_Book")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("a0855758-075e-45bf-aee9-21a1849e1a1b")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
