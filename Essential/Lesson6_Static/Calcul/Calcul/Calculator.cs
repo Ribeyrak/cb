0124c1dd4ed8e0283b42d04b67c8b4a2786f3a31
Ribeyrak <ev.lukhtan@gmail.com>
2018-02-28 17:31:25 +0200

    Final

---
+++ Essential/Lesson6_Static/Calcul/Calcul/Calculator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Calcul
+{
+    internal static class Calculator
+    {
+        public static double Addition(double a, double b)
+        {
+            return a + b;
+        }
+
+        public static double Subctraction(double a, double b)
+        {
+            return a - b;
+        }
+
+        public static double Multiplication(double a, double b)
+        {
+            return a * b;
+        }
+
+        public static double Division(double a, double b)
+        {
+            if (b == 0) Console.WriteLine("На ноль делить нельзя");
+            return a / b;
+        }
+    }
+}
--- Essential/Lesson6_Static/Calcul/Calcul/Program.cs
+++ Essential/Lesson6_Static/Calcul/Calcul/Program.cs
@@ -1,15 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Calcul
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            Console.WriteLine(Calculator.Addition(12, 15));
+            Console.WriteLine(Calculator.Subctraction(46.34, 11.5));
+            Console.WriteLine(Calculator.Multiplication(2.5, 10));
+            Console.WriteLine(Calculator.Division(5, 0));
+
+            Console.ReadKey();
         }
     }
 }
