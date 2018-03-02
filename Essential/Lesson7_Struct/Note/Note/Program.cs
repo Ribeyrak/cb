bad0bea4ad1fb33d98748b56c2e951346662b2e0
Ribeyrak <ev.lukhtan@gmail.com>
2018-03-02 19:58:11 +0200

    start task #1

---
+++ Essential/Lesson7_Struct/Note/Note/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Note
+{
+    struct Notebook
+    {
+        private int model;
+        private string manufacturer;
+        private decimal price;
+
+        public Notebook(int model, string manufacturer, decimal price)
+        {
+            this.model = model;
+            this.manufacturer = manufacturer;
+            this.price = price;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine("Test Show");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
