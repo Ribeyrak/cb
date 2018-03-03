d9795e04043e451ee6a11420c48fbbd46bde3dd5
Ribeyrak <ev.lukhtan@gmail.com>
2018-03-03 14:06:47 +0200

    Holdback

---
+++ Essential/Lesson7_Struct/Timetable/Timetable/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Timetable
+{
+    struct Train
+    {
+        private string _destination;
+        private int _trainNumber;
+        private int _departureTime;
+        public Train(string destination, int trainNumber, int departureTime)
+        {
+            _destination = destination;
+            _trainNumber = trainNumber;
+            _departureTime = departureTime;
+        }
+
+        public string Destination
+        {
+            get { return _destination; }
+        }
+
+        public int TrainNumber
+        {
+            get { return _trainNumber; }
+        }
+
+        public int DepartureTime
+        {
+            get { return _departureTime; }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
