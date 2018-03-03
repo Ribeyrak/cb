3584ca138bab568e23b174b4e4effc2016a2211f
Ribeyrak <ev.lukhtan@gmail.com>
2018-03-03 15:33:54 +0200

    End of task

---
+++ Essential/Lesson7_Struct/Class_Struct/Class_Struct/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Class_Struct
+{
+    class MyClass
+    {
+        public string change;
+    }
+
+    struct MyStruct
+    {
+        public string change;
+    }
+    class Program
+    {
+        static void ClassTaker(MyClass myClass)
+        {
+            myClass.change = "изменено";
+        }
+
+        static void StrucktTaker(MyStruct myStruct)
+        {
+            myStruct.change = "изменено";
+        }
+
+        static void Main(string[] args)
+        {
+            MyClass _class = new MyClass();
+            MyStruct _struct = new MyStruct();
+
+            _class.change = "не изменено";
+            _struct.change = "не изменено";
+
+            ClassTaker(_class);
+            StrucktTaker(_struct);
+
+            Console.WriteLine("Поле класа {0} ",_class.change);
+            Console.WriteLine("Поле структуры {0}",_struct.change);
+
+            Console.ReadKey();
+        }
+    }
+}
