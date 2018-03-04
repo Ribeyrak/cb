9c1d869341933be3c9197d1bded4b0473c1baffa
Ribeyrak <ev.lukhtan@gmail.com>
2018-03-04 19:32:00 +0200

    use Google for help

---
+++ Essential/Lesson8_Struct/Printer/Printer/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Printer
+{
+    enum Colors
+    {
+        Red,
+        Yellow,
+        Green,
+        Blue,
+   }
+
+    static class MyClass
+    {
+        static void Print(string stroka, int color)
+        {
+            stroka = Console.ReadLine();
+            switch (color)
+            {
+                case (int)Colors.Red:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    break;
+                case (int)Colors.Yellow:
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    break;
+                case (int)Colors.Green:
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    break;
+                case (int)Colors.Blue:
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    break;
+            }
+
+            Console.BackgroundColor = ConsoleColor.Cyan;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tets");
+
+            Console.ReadKey();
+        }
+    }
+}
