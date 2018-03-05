50246a857d22e85a3d5fc4b2e1cdadcf25987299
Ribeyrak <ev.lukhtan@gmail.com>
2018-03-05 18:17:26 +0200

    start

---
+++ Essential/Lesson8_Struct/Worker/Worker/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Worker
+{
+    enum Position
+    {
+        Trainee = 0,
+        Seller = 1,
+        Manager = 2,
+        Director = 3
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
