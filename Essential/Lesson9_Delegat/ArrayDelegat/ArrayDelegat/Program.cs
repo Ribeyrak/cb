01b84de2f8bf1b484d285ae27710f23f9d598da6
Ribeyrak <ev.lukhtan@gmail.com>
2018-03-08 23:19:32 +0200

    что-то боязнь у меня этих массивов

---
+++ Essential/Lesson9_Delegat/ArrayDelegat/ArrayDelegat/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayDelegat
+{
+    delegate int MyDelegate();
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
