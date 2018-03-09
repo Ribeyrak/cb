d8a63c15fd808fb2ced01cc5ca0c14d63d2e889a
Ribeyrak <ev.lukhtan@gmail.com>
2018-03-10 01:07:46 +0200

    Совсем не понял суть задания

---
+++ Essential/Lesson10_Generics/GenFabMeth/GenFabMeth/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace GenFabMeth
+{
+    internal class MyClass<T> where T : new()
+    {
+        public static T FactoryMethod()
+        {
+            return new T();
+        }
+    }
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int i = MyClass<int>.FactoryMethod();
+
+            Console.WriteLine(i.GetType().Name);
+            Program p = MyClass<Program>.FactoryMethod();
+
+            Console.WriteLine(p.GetType().Name);
+
+            Console.ReadKey();
+        }
+    }
+}
