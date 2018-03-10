0975a830cb6c92f49712ac9165002a5ebdde7412
Ribeyrak <ev.lukhtan@gmail.com>
2018-03-10 02:07:20 +0200

    Все списал. Почти все моменты не понятны

---
+++ Essential/Lesson10_Generics/GenericLikeList/GenericLikeList/IMyList.cs
@@ -0,0 +1,10 @@
+﻿namespace GenericLikeList
+{
+    internal interface IMyList<T>
+    {
+        void Add(T a);
+        T this[int index] { get; }
+        int Count { get; }
+        bool Contains(T item);
+    }
+}
